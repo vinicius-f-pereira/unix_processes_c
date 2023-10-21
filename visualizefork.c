6ef88eb472a719342d021a29bc87b6709f39de6c
Vinicius Pereira <niviciusdev@gmail.com>
2023-10-21 00:09:48 -0300

    fix: updated file with indentation  Changes to be committed: 	modified:   visualizefork.c

--- visualizefork.c
+++ visualizefork.c
@@ -1,20 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   visualizefork.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 00:09:11 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/10/21 00:09:32 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/wait.h>
 
-int	main(int argc, char* argv[])
+int	main(int argc, char *argv[])
 {
-	int	id = fork();
+	int	id;
 	int	n;
+	int	i;
+
+	id = fork();
 	if (id == 0)
 		n = 1;
 	else 
 		n = 52;
 	if (id != 0)
 		wait();
-
-	int i;
-		for (i = n; i < n + 50; i++)
+	for (i = n; i < n + 50; i++)
 	{
 		printf("%d ", i);
 		fflush(stdout);
