7f7a6633247f42b815705729db7eba82558eaaf6
Vinicius Pereira <niviciusdev@gmail.com>
2023-10-21 21:43:51 -0300

    feat: implemented muchforks  Changes to be committed: 	new file:   muchforks 	modified:   muchforks.c

--- muchforks.c
+++ muchforks.c
@@ -3,12 +3,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int	main(int argc, char *argv[])
 {
-	int	id;
+	int	id1;
 	int	id2;
 
-	id = fork();
+	id1 = fork();
 	id2 = fork();
+
+	if (id1 == 0)
+	{
+		if (id2 == 0)
+		{
+			printf("We are process y\n");
+		}
+		else
+			printf("We are process x\n");
+	}
+	else
+	{
+		if (id2 == 0)
+		{
+			printf("We are process z\n");
+		}
+		else
+		{
+			printf("We are the parent process\n");
+		}
+	}
+	while (wait(NULL) != -1 || errno != ECHILD)
+	{
+		printf("Waited for a child to finish\n");
+	}
+	return (0);
 }
