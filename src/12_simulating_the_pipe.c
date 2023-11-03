993a52df47a078435e5bdb5b3ccfbae7feb28834
vde-frei <vde-frei@student.42sp.org.br>
2023-11-02 23:19:21 -0300

    feat: implemented pipe command without execve Changes to be committed: 	modified:   src/12_simulating_the_pipe.c

--- src/12_simulating_the_pipe.c
+++ src/12_simulating_the_pipe.c
@@ -7,17 +7,39 @@
 int	main(int argc, char **argv)
 {
 	int	fd[2];
-	int	pid;
+	int	pid1;
+	int	pid2;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		printf("in fucker child\n");
+	if(pipe(fd) == -1) {
+		return (1);
 	}
-	else
-	{
-		wait(NULL);
-		printf("in fucker child\n");
+	pid1 = fork();
+	if (pid1 < 0) {
+		return 2;
 	}
+	if (pid1 == 0) {
+		// child process 1 (ping)
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ping", "ping", "-c", "5", "google.com", NULL);
+	}
+	pid2 = fork();
+	if (pid2 < 0) {
+		return (3);
+	}
+	if (pid2 == 0) {
+		// Child process 2 (grep)
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "rtt", NULL);
+	}
+	// Need to close fd, because it still open in parent process
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	printf("Done\n");
 	return (0);
 }
