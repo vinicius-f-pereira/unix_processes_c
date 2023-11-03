4978dd15e54cd307578410ff721a77f9160c91ef
vde-frei <vde-frei@student.42sp.org.br>
2023-11-02 22:40:26 -0300

    fix: started to implement function

--- src/12_simulating_the_pipe.c
+++ src/12_simulating_the_pipe.c
@@ -6,6 +6,18 @@
 
 int	main(int argc, char **argv)
 {
+	int	fd[2];
+	int	pid;
 
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("in fucker child\n");
+	}
+	else
+	{
+		wait(NULL);
+		printf("in fucker child\n");
+	}
 	return (0);
 }
